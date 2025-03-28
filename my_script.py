a141b174625203896544cb34ff378e7f29b5d8b7
Gabriel LEGROS <165050056+gabriel-legros-ensta@users.noreply.github.com>
2025-03-28 08:56:37 +0100

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -17,18 +17,18 @@ video_path = 'basket.mp4'
 cap = cv2.VideoCapture(video_path)
 
 # Load a model
-#model = YOLO("yolo11n.pt").to(device)
+model = YOLO("yolo11n.pt").to(device)
 print(f"Modèle chargé sur : {device}")
 
 """# **Entrainement**"""
 
-#model.train(data="dataset3/data.yaml", epochs=200, cache=True, device="cuda", batch=32, imgsz=640, patience=20)
-#device="cuda"
+model.train(data="dataset/data.yaml", epochs=200, cache=True, cos_lr=True, device="cuda", batch=32, imgsz=640, patience=20)
+device="cuda"
 
 """# **Validation**"""
 
 # Charger le modèle entraîné
-model = YOLO('runs/detect/train5/weights/best.pt').to(device)
+model = YOLO('runs/detect/train/weights/best.pt').to(device)
 print(f"Modèle chargé sur : {device}")
 
 # Effectuer l'évaluation sur le jeu de validation
@@ -42,38 +42,39 @@ print(f"Modèle chargé sur : {device}")
 
 """# **Video**"""
 # Obtenir les informations de la vidéo (largeur, hauteur, FPS)
-fps = cap.get(cv2.CAP_PROP_FPS)
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#fps = cap.get(cv2.CAP_PROP_FPS)
+#frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Créer un objet VideoWriter pour enregistrer la vidéo de sortie
-out = cv2.VideoWriter('output_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
+# out = cv2.VideoWriter('output_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     if not ret:
+#         break
     
-    # Convertir l'image en format RGB pour YOLO
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     # Convertir l'image en format RGB pour YOLO
+#     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Faire une prédiction avec YOLO
-    results = model(frame_rgb)
+#     # Faire une prédiction avec YOLO
+#     results = model(frame_rgb)
 
-    # Afficher les résultats sur l'image
-    annotated_frame = results[0].plot()  # Dessine les boîtes de détection sur l'image
+#     # Afficher les résultats sur l'image
+#     annotated_frame = results[0].plot()  # Dessine les boîtes de détection sur l'image
 
-    # Convertir l'image annotée en format BGR pour OpenCV
-    annotated_frame_bgr = cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR)
+#     # Convertir l'image annotée en format BGR pour OpenCV
+#     annotated_frame_bgr = cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR)
 
-    # Enregistrer la frame annotée
-    out.write(annotated_frame_bgr)
+#     # Enregistrer la frame annotée
+#     out.write(annotated_frame_bgr)
 
-    # Afficher la vidéo (optionnel)
-    #cv2.imshow('Frame', annotated_frame_bgr)
+#     # Afficher la vidéo (optionnel)
+#     # cv2.imshow('Frame', annotated_frame_bgr)
 
 
-# Libérer les ressources
-cap.release()
-out.release()
-#cv2.destroyAllWindows()
+# # Libérer les ressources
+# cap.release()
+# out.release()
+# # cv2.destroyAllWindows()
+
